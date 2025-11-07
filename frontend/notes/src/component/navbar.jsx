dc269f5ddc40d0f2a79f56e762bf29e0376acb67
umesh gupta <umeshg2059@gmail.com>
2025-11-07 15:43:29 +0530

    dsfk

--- frontend/notes/src/component/navbar.jsx
+++ frontend/notes/src/component/navbar.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 
 
 const Navbar=()=>{
+
+  const handleUploadclick=()=>{
+    alert("button clickable")
+  }
     return(
   <nav className="flex items-center justify-between px-6 py-4 bg-yellow-50 shadow-sm relative z-50">
    <h1 className="text-2xl font-bold text-black">
     open<span className="text-green-600">Study</span>
    </h1>
    <div className="flex items-center gap-3">
-    <button className="border rounded-full px-4 py-2 bg-green-100">Ask Doubts</button>
-    <button className="bg-green-500 text-white rounded-full px-4 py-2">Upload Notes</button>
+    <button className="border rounded-full px-4 py-2 bg-green-100" 
+    onClick={handleUploadclick}>Ask Doubts</button>
+    <button className="bg-green-500 text-white rounded-full px-4 py-2"
+    onClick={handleUploadclick}>Upload Notes</button>
     <img src="/image1.png" className="w-10 h-10 rounded-full" />
   </div>
 </nav>
- 
 
  )
 }
